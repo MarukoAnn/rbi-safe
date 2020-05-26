8389c829a3c58db6a291d4505f32e5562c657146
DestinyJun <429771890@qq.com>
2020-05-26 15:49:47 +0800

    操作日志： （1）培训内容设置一 ---- 2020-05-26 15:49 ----

--- src/app/business/safetrain/st-plain/plain-edit/pl-input/pl-input.component.ts
+++ src/app/business/safetrain/st-plain/plain-edit/pl-input/pl-input.component.ts
@@ -10,31 +10,34 @@ import {GlobalService} from '../../../../../common/services/global.service';
 })
 export class PlInputComponent implements OnInit {
   // @ViewChild(Calendar, {static: true}) calendar: Calendar;
-  public cars: EveryCategory[];
-  public selectedCar1: EveryCategory;
-  public startDate: Date;
-  public sendDate: Date;
-  public display: boolean = false;
-  public es: any;
-  public dataTrees: OrgTree[] = [];
-  public dataTree: OrgTree = {};
+  public iptDropdownOptions: EveryCategory[];
+  public iptDropdownSelected: EveryCategory;
+  public iptStartDate: Date;
+  public ipTendDate: Date;
+  public iptOrgModal: boolean = false;
+  public iptEs: any = Es;
+  public iptOrgTree: OrgTree[] = [];
+  public iptOrgTreeSelect: OrgTree = {};
+  public iptInputFiled: any = {
+    title: '',
+    duration: null
+  };
   constructor(
     private globalSrv: GlobalService,
   ) { }
   ngOnInit() {
-    this.cars = [
+    this.iptDropdownOptions = [
       {name: '全部', value: '1', flag: 0},
       {name: 'Audi', value: 'Audi', flag: 0},
       {name: 'BMW', value: 'BMW', flag: 1},
     ];
-    this.es = Es;
     this.globalSrv.getOrgazitionTreeData().subscribe(
       (res) => {
-        this.dataTrees = orgInitializeTree(res.data);
+        this.iptOrgTree = orgInitializeTree(res.data);
       }
     );
   }
   public showDialog() {
-    this.display = true;
+    this.iptOrgModal = true;
   }
 }
--- src/app/business/safetrain/st-plain/plain-edit/pl-train/pl-train.component.ts
+++ src/app/business/safetrain/st-plain/plain-edit/pl-train/pl-train.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pl-train.component.scss']
 })
 export class PlTrainComponent implements OnInit {
-
+  public traTabActiveIndex: number = 0;
   constructor() { }
 
   ngOnInit() {
